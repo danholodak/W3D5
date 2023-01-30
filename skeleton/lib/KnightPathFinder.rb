1885548d7e0949f760ebf42282a3b8fb4fb2d927
Dan Holodak <danholodak@gmail.com>
2023-01-30 11:10:26 -0500

    Finish KnightPathFinder.rb

--- skeleton/lib/KnightPathFinder.rb
+++ skeleton/lib/KnightPathFinder.rb
@@ -9,17 +9,20 @@ class KPFinder
         @accounted_move = [pos]
     end
 
-    def build_possible_moves(pos)
+    def build_possible_moves(pos=nil)
+       pos ||= [self.row, self.col]
         x, y = pos
-        @possible_moves << PolyTreeNode.new([x + 2, y + 1]) if valid_pos?([x + 2, y + 1])
-        @possible_moves << PolyTreeNode.new([x - 2, y + 1]) if valid_pos?([x - 2, y + 1])
-        @possible_moves << PolyTreeNode.new([x + 2, y - 1]) if valid_pos?([x + 2, y - 1])
-        @possible_moves << PolyTreeNode.new([x - 1, y + 2]) if valid_pos?([x - 1, y + 2])
-        @possible_moves << PolyTreeNode.new([x - 2, y - 1]) if valid_pos?([x - 2, y - 1])
-        @possible_moves << PolyTreeNode.new([x + 1, y - 2]) if valid_pos?([x + 1, y - 2])
-        @possible_moves << PolyTreeNode.new([x - 1, y - 2]) if valid_pos?([x - 1, y - 2])
-        @possible_moves << PolyTreeNode.new([x + 1, y + 2]) if valid_pos?([x + 1, y + 2])
-        
+        new_moves = []
+        new_moves << PolyTreeNode.new([x + 2, y + 1]) if valid_pos?([x + 2, y + 1])
+        new_moves << PolyTreeNode.new([x - 2, y + 1]) if valid_pos?([x - 2, y + 1])
+        new_moves << PolyTreeNode.new([x + 2, y - 1]) if valid_pos?([x + 2, y - 1])
+        new_moves << PolyTreeNode.new([x - 1, y + 2]) if valid_pos?([x - 1, y + 2])
+        new_moves << PolyTreeNode.new([x - 2, y - 1]) if valid_pos?([x - 2, y - 1])
+        new_moves << PolyTreeNode.new([x + 1, y - 2]) if valid_pos?([x + 1, y - 2])
+        new_moves << PolyTreeNode.new([x - 1, y - 2]) if valid_pos?([x - 1, y - 2])
+        new_moves << PolyTreeNode.new([x + 1, y + 2]) if valid_pos?([x + 1, y + 2])
+        @possible_moves.concat(new_moves)
+        return(new_moves)
     end
 
     # def link_parent
@@ -31,15 +34,21 @@ class KPFinder
     # end
 
     def build_move_tree
+        self.build_possible_moves
         # queue = @possible_moves
         while !@possible_moves.empty? do
             current = @possible_moves.shift
-            if @accounted_move.include?(move)
+
+            if @accounted_move.include?(current.value)
                 next
             else
-                build_possible_moves(current.value)
-            
-
+                @accounted_move << current.value
+                build_possible_moves(current.value).each do |move|
+                    move.parent = current
+                end
+                
+            end
+        end
 
 
     end
